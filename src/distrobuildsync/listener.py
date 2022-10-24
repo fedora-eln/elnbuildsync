23527d0138e2613e232da08c977305f24c310d67
Stephen Gallagher <sgallagh@redhat.com>
2022-10-24 16:45:20 -0400

    Fix crash in build_components
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- src/distrobuildsync/listener.py
+++ src/distrobuildsync/listener.py
@@ -175,12 +175,7 @@ def build_components(target, builds):
 
     with bsys.multicall(batch=config.koji_batch) as mc:
         for rd in builds:
-            # We occasionally crash here for unknown reasons,
-            # so we'll report it and continue instead.
-            if not hasattr(rd, "comp"):
-                logger.critical("BUG: crashed on rd={}".format(rd))
-                continue
-            if config.is_eligible(rd.comp):
+            if config.is_eligible(rd.ns, rd.comp):
                 namespace = rd.ns
                 scmurl = config.split_scmurl(rd.scmurl)
                 gitcomponent = scmurl["comp"]
