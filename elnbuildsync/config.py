30c3aaaae68a279a21bdc78453ec6a14154fca7b
Stephen Gallagher <sgallagh@redhat.com>
2023-08-21 19:36:52 -0400

    Add timeout for build tasks
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- elnbuildsync/config.py
+++ elnbuildsync/config.py
@@ -41,6 +41,7 @@ config_timer = 15 * 60  # 15 minutes
 cleanup_timer = 12 * 60 * 60  # 12 hours
 status_timer = 10 * 60  # 10 minutes
 task_check_timer = 5 * 60  # 5 minutes
+task_timeout = 24 * 60 * 60  # 24 hours
 koji_batch = 500
 configuration = None
 config_ref = None
