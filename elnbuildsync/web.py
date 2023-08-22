ae20d3cf321876f1c16fa9ba69c23cb6a219968e
Stephen Gallagher <sgallagh@redhat.com>
2023-08-22 13:32:22 -0400

    Add ability to trigger builds via POST message
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- elnbuildsync/web.py
+++ elnbuildsync/web.py
@@ -18,13 +18,18 @@
 
 # SPDX-License-Identifier: 	GPL-3.0-or-later
 
+import json
 import logging
 
 from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.task import deferLater
+from twisted.web.error import Error as WebError
 from twisted.web.resource import Resource
-from twisted.web.server import Site
+from twisted.web.server import Site, NOT_DONE_YET
 from twisted.web.util import Redirect
 
+from . import batching
 from . import status
 
 logger = logging.getLogger(__name__)
@@ -138,20 +143,83 @@ class StatusPageResource(Resource):
         return status.web_page
 
 
+class TriggerBuildResource(Resource):
+    """
+    TriggerBuildResource
+
+    Accepts a POST request containing a JSON list of components to rebuild for
+    ELN.
+    """
+
+    isLeaf = True
+
+    def getChild(self, name, request):
+        if name == "":
+            return self
+        return Resource.getChild(self, name, request)
+
+    @staticmethod
+    def _simple_yield():
+        pass
+
+    def _done(self, data):
+        self.request.finish()
+
+    def _failed(self, data):
+        logger.critical(data)
+        self.request.finish()
+
+    def render_POST(self, request):
+        self.request = request
+
+        deferred = deferLater(reactor, 0, self._do_post)
+        deferred.addCallback(self._done)
+        deferred.addErrback(self._failed)
+        return NOT_DONE_YET
+
+
+    @inlineCallbacks
+    def _do_post(self):
+        global started
+        yield self._simple_yield()
+
+        self.request.setHeader("Cache-Control", "no-cache")
+        if not started:
+            self.request.setResponseCode(503)
+
+
+        content_type = self.request.getHeader("Content-Type")
+        if not content_type or content_type != "application/json":
+            self.request.setResponseCode(415)
+            self.request.write(b"Unsupported Content-Type\n")
+            raise WebError("Invalid Content-Type")
+
+        # Read in the content
+        # TODO: This is insecure! Do something about this.
+        components = json.load(self.request.content)
+
+        yield batching.rebuild_from_components(components)
+
+
 def setup_web_resources():
     global started
-    started = True
     root = RootResource()
     root.putChild(b"startup", StartupResource())
     root.putChild(b"alive", LivenessResource())
     root.putChild(b"status.json", StatusJSONResource())
     root.putChild(b"status.html", StatusPageResource())
     root.putChild(b"status", Redirect(b"status.html"))
+    root.putChild(b"trigger", TriggerBuildResource())
+    started = True
 
     return Site(root)
 
 
 if __name__ == "__main__":
     # For debugging
+    logging.basicConfig(
+        format="%(asctime)s : %(name)s : %(levelname)s : %(message)s",
+        level=logging.DEBUG,
+    )
     reactor.listenTCP(8080, setup_web_resources())
     reactor.run()
