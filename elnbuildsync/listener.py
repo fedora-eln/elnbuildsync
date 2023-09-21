19716a41c25dd59a44a43e1c35c586757cdd0ed8
Stephen Gallagher <sgallagh@redhat.com>
2023-09-21 10:08:53 -0400

    Use correct timeout value for tasks
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- elnbuildsync/listener.py
+++ elnbuildsync/listener.py
@@ -225,7 +225,7 @@ def fire_errback(deferred, *data):
         pass
 
 
-def register_build_task_id(task_id, timeout=config.task_check_timer):
+def register_build_task_id(task_id, timeout=config.task_timeout):
     logger.debug(f"Registering task {task_id}")
     if task_id in state.active_builds:
         raise ValueError("Cannot register the same task ID twice")
