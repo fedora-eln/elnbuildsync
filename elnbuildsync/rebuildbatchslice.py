cca474defb402f71ba85f199e5730d5c9e6d746e
Stephen Gallagher <sgallagh@redhat.com>
2026-07-24 13:00:23 -0400

    Drop redundant assignment
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- elnbuildsync/rebuildbatchslice.py
+++ elnbuildsync/rebuildbatchslice.py
@@ -107,9 +107,7 @@ class RebuildBatchSlice:
         # Set up the RebuildAttempt
         all_successes = {}
         scm_urls = [msg.scmurl for msg in self.tag_messages]
-        attempt = attempt = await RebuildAttempt(
-            scm_urls=scm_urls, slice=self
-        ).async_init()
+        attempt = await RebuildAttempt(scm_urls=scm_urls, slice=self).async_init()
 
         successes, failures = await attempt.async_await()
 
