34db457786002e385a49ac6ae54617ace65ec828
Stephen Gallagher <sgallagh@redhat.com>
2026-09-18 13:37:28 -0400

    Add instance differentiation for Koji
    There may be multiple Koji instances communicating with the same routing
    keys (such as the Fedora primary Koji and a secondary one for
    new-architecture bring-up work). Add support for checking the
    `body.instance` value in the messages received to ensure they come from
    the instance we want to work with.
    
    Fixes: https://github.com/fedora-eln/elnbuildsync/issues/81
    
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- tests/integration/harness.py
+++ tests/integration/harness.py
@@ -139,6 +139,7 @@ async def build_harness(
     bodhi_batch_size: int = 0,
     bodhi_max_single_batch_size: int | None = None,
     bodhi_warn_timeout: float | None = None,
+    koji_instance: str = "primary",
     tag_timeout: float | None = None,
     stable_timeout: float | None = None,
     task_timeout: float | None = None,
@@ -203,6 +204,7 @@ async def build_harness(
                 "stable_tag": STABLE_TAG,
                 "scratch_build": scratch_build,
                 "fail_fast": fail_fast,
+                "instance": koji_instance,
             },
             "bodhi": bodhi_config,
             "db": {
@@ -270,7 +272,7 @@ async def build_harness(
         )
 
     loop = asyncio.get_running_loop()
-    bus = FakeMessageBus()
+    bus = FakeMessageBus(instance=koji_instance)
 
     fake_koji = FakeKojiClientSession(loop, bus)
     fake_koji.set_build_target(BUILD_TARGET, BUILD_TAG_NAME, DEST_TAG_NAME)
